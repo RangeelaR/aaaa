3137e02c973279eb38ce3dde62a400c271c36e19
RangeelaR <35609684+RangeelaR@users.noreply.github.com>
2018-08-14 11:41:02 +0530

    Update and rename Rount.java to ount.java

---
+++ ount.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+public class ount {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		long a= sc.nextInt();
+		String b= String.valueOf(a);
+		int n=b.length();
+		System.out.println("number count"+n);
+		
+
+	}
+
+}
